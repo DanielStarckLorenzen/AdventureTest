56a4303a04bd25b2719db729162b166a5be48d8b
emil-KEA <emil33v4@stud.kea.dk>
2022-03-15 12:11:25 +0100

    with getRoom

--- src/GameTest.java
+++ src/GameTest.java
@@ -28,14 +28,21 @@ public class GameTest {
     }
 
     void go() {
-        userInterface();
-      /*  Room currentRoom = new Room("A Table, a chair and a broken lamp");
-        Room room1 = new Room("");
-        Room room2 = new Room("");
-        Room room3 = new Room("");
-        Room room4 = new Room("");
-        Room room5 = new Room("");
-       */
+        //userInterface();
+    Room room1 = new Room();
+    Room room2 = new Room();
+    Room room3 = new Room();
+    Room room4 = new Room();
+    Room room5 = new Room();
+    Room room6 = new Room();
+    Room room7 = new Room();
+    Room room8 = new Room();
+    Room room9 = new Room();
+    room1.setEast(room2);
+    room1.setSouth(room4);
+
+
+
 
     }
 
--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+/*
 public class Room {
     private String direction;
     private String look;
@@ -10,7 +10,39 @@ public class Room {
     Room() {
 
         }
-}
 
+
+} */
+public class Room{
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+    private String name;
+    private String listOfThings;
+
+    Room(){};
+    Room(String name, String listOfThings){
+        this.name = name;
+        this.listOfThings = listOfThings;
+    };
+
+    void setNorth(Room north){
+        this.north = north;
+    }
+    void setSouth(Room south){
+        this.south = south;
+    }
+    void setEast(Room east){
+        this.east = east;
+    }
+    void setWest(Room west){
+        this.west = west;
+    }
+    String getListOfThings(){
+        return listOfThings;
+    }
+
+}
 
 
