bb26167c23d0fcd21041e7ba943f6d0b751dbd0a
AugustG01 <augu8204@stud.kea.dk>
2022-03-15 12:17:44 +0100

    opdateret rooms

--- src/GameTest.java
+++ src/GameTest.java
@@ -40,10 +40,21 @@ public class GameTest {
     Room room9 = new Room();
     room1.setEast(room2);
     room1.setSouth(room4);
-
-
-
-
+    room2.setEast(room3);
+    room2.setWest(room1);
+    room3.setWest(room2);
+    room3.setSouth(room6);
+    room6.setSouth(room9);
+    room6.setNorth(room3);
+    room9.setNorth(room6);
+    room9.setWest(room8);
+    room8.setNorth(room5);
+    room8.setWest(room7);
+    room8.setEast(room9);
+    room7.setEast(room8);
+    room7.setNorth(room4);
+    room4.setSouth(room7);
+    room4.setNorth(room1);
     }
 
     public static void main(String[] args) {
--- src/Room.java
+++ src/Room.java
@@ -43,6 +43,8 @@ public class Room{
         return listOfThings;
     }
 
+
+
 }
 
 
