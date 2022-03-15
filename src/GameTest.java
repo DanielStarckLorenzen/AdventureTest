d56acfc4160e79668b95ffc932f3da22dad387c8
daniellorenzen <dani141d@stud.kea.dk>
2022-03-15 15:06:11 +0100

    RoomsFinalMaybe

--- src/GameTest.java
+++ src/GameTest.java
@@ -90,10 +90,10 @@ public class GameTest {
     }
 
     void gameSetup(){
-        Room room1 = new Room("Room 1","You stand in front of a decaying house, and spot two doors");
-        Room room2 = new Room("Room 2","You entered the main entrance, there is broken glass all over the ground," +"" +
+        Room room1 = new Room("The Field","You stand in front of a decaying house, and spot two doors");
+        Room room2 = new Room("Main Entrance","You entered the main entrance, there is broken glass all over the ground," +"" +
                 "among the shattered glass, are broken picture frames");
-        Room room3 = new Room("Room 3","You entered the dining room, there is a big old clock which is very loud,"+
+        Room room3 = new Room("Dining Room","You entered the dining room, there is a big old clock which is very loud,"+
                 " dining table has mold growing on it");
         Room room4 = new Room("Back entrance", "You are standing on a rotten carpet." +
                 "A bit of light enters through a broken window." +
@@ -103,12 +103,12 @@ public class GameTest {
         Room room6 = new Room("The Lounge", "There are no windows in this room." +
                 "The only light is a small candlelight in the southeastern corner." +
                 "A door to the south is ajar.");
-        Room room7 = new Room("Room 7","The room seems to be a bedroom but it's very dark. " +
+        Room room7 = new Room("Bedroom","The room seems to be a bedroom but it's very dark. " +
                 "You can feel cobweb all over you and see a little reflection near the night stand.");
-        Room room8 = new Room("Room 8","You are in what seems to be the kitchen. " +
+        Room room8 = new Room("Kitchen","You are in what seems to be the kitchen. " +
                 "It's very smokey and hard to see. " +
                 "You can faintly see a staircase but can't put a finger on the which way to get to it.");
-        Room room9 = new Room("Room 9","Room 9 seems to be the bathroom. " +
+        Room room9 = new Room("Bathroom","Room 9 seems to be the bathroom. " +
                 "It's very cold since a small window is broken and banging against the outside bricks. " +
                 "You see the medicine closet is slightly open.");
 
