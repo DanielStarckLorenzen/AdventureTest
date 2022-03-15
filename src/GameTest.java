77c07c913fd55155497b3d3cb767782da52a9178
emil-KEA <emil33v4@stud.kea.dk>
2022-03-15 14:47:12 +0100

    with description

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,39 +1,39 @@
 import java.util.Scanner;
 
 public class GameTest {
-    private String direction;
-    private String look;
-    private String name;
     Room currentRoom;
+    boolean gameOn = true;
 
 
     Scanner scan = new Scanner(System.in);
 
     void intro() {
+        String name;
         System.out.print("What is your name? ");
         name = scan.nextLine();
         System.out.println("Hi " + name + " ");
     }
 
     void look() {
-
+        System.out.println(currentRoom.getListOfThings());
     }
 
     void help() {
-        System.out.println("Type \"Go north/south/east/west\" to go that way" +
-                "\nType \"look\" to look at your surroundnings" +
-                "\nType \"Exit\" to exit the game");
+        System.out.println("""
+                Type "Go north/south/east/west" to go that way
+                Type "look" to look at your surroundnings
+                Type "Exit" to exit the game""");
         userInterface();
     }
 
     void userInterface() {
         System.out.println("What do you want to do?" +
                 "\nType help to get commands");
-        String answer = scan.nextLine();
+        String answer = scan.nextLine().toLowerCase();
 
         switch (answer) {
             case "help" -> help();
-            case "exit" -> System.out.println("You exited the program");
+            case "exit" -> exit();
             case "look" -> look();
             case "north", "n", "go north" -> goNorth();
             case "south", "s", "go south" -> goSouth();
@@ -46,7 +46,9 @@ public class GameTest {
     void go() {
         gameSetup();
         intro();
-        userInterface();
+        while(gameOn) {
+            userInterface();
+        }
 
     }
 
@@ -55,6 +57,7 @@ public class GameTest {
             currentRoom = currentRoom.getNorth();
             System.out.println("You go north");
             System.out.println("Entering " + currentRoom.getName());
+            look();
         } else doesNotExist();
     }
     void goSouth(){
@@ -62,6 +65,7 @@ public class GameTest {
             currentRoom = currentRoom.getSouth();
             System.out.println("You go south");
             System.out.println("Entering " + currentRoom.getName());
+            look();
         } else doesNotExist();
     }
     void goEast(){
@@ -69,6 +73,7 @@ public class GameTest {
             currentRoom = currentRoom.getEast();
             System.out.println("You go east");
             System.out.println("Entering " + currentRoom.getName());
+            look();
         } else doesNotExist();
     }
     void goWest(){
@@ -76,6 +81,7 @@ public class GameTest {
             currentRoom = currentRoom.getWest();
             System.out.println("You go west");
             System.out.println("Entering " + currentRoom.getName());
+            look();
         } else doesNotExist();
     }
     void doesNotExist(){
@@ -87,9 +93,14 @@ public class GameTest {
         Room room1 = new Room();
         Room room2 = new Room();
         Room room3 = new Room();
-        Room room4 = new Room();
-        Room room5 = new Room();
-        Room room6 = new Room();
+        Room room4 = new Room("Back entrance", "You are standing on a rotten carpet." +
+                "A bit of light enters through a broken window." +
+                "A sweet smell of decay comes from the the southern door");
+        Room room5 = new Room("The dungeon", "All dark. You have no sense of direction." +
+                "Some heavy creature is breathing nearby. It seems to be coming your way.");
+        Room room6 = new Room("The Lounge", "There are no windows in this room." +
+                "The only light is a small candlelight in the southeastern corner." +
+                "A door to the south is ajar.");
         Room room7 = new Room();
         Room room8 = new Room();
         Room room9 = new Room();
@@ -112,6 +123,10 @@ public class GameTest {
         room4.setNorth(room1);
 
         currentRoom = room1;
+    }
+    void exit(){
+        System.out.println("You exited the program");
+        gameOn = false;
     }
 
     public static void main(String[] args) {
